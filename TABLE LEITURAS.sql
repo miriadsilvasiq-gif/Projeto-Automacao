b41bb2df70590483a89b447f2334b392f8b52648
miriadsilvasiq-gif <miriadsilvasiq@gmail.com>
2026-03-26 14:11:54 -0300

    Delete TABLE LEITURAS.sql

--- TABLE LEITURAS.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE leituras (
-    id_leitura INT AUTO_INCREMENT PRIMARY KEY,
-    valor DECIMAL(5,2),
-    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    id_sensor INT,
-    FOREIGN KEY (id_sensor) REFERENCES sensores(id_sensor)
-);
